3b2e26d20bbde30834eff61f8bcad7342138e717
Anne Fortuin <phortuin@gmail.com>
2020-08-31 16:21:33 +0200

    Initial commit.

---
+++ index.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+
+const unified = require('unified')
+const stream = require('unified-stream')
+const markdown = require('remark-parse')
+const remark2rehype = require('remark-rehype')
+const prism = require('@mapbox/rehype-prism')
+const doc = require('rehype-document')
+const html = require('rehype-stringify')
+const inline = require('rehype-inline')
+const minify = require('rehype-preset-minify')
+
+const processor = unified()
+	.use(markdown)
+	.use(remark2rehype)
+	.use(prism)
+	.use(doc, {
+		title: 'Readme', // file name?
+		css: 'index.css',
+	})
+	.use(inline)
+	.use(minify)
+	.use(html)
+
+process.stdin
+	.pipe(stream(processor))
+	.pipe(process.stdout)
---
+++ readme.md
@@ -0,0 +1,13 @@
+# Downpipe
+
+> Command line utility to convert Markdown to HTML, including inlined CSS
+
+This is a very simple CLI program that takes a stream of markdown, converts it to a valid HTML document with inlined CSS styles and outputs as a string.
+
+Usage:
+
+```bash
+$ downpipe < readme.md > index.html
+```
+
+Uses Unified & Rehype plus several plugins.
